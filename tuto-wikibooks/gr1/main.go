2c19be791360f88e31b73c96f62f1e15940354a4
Sebastien Binet <binet@cern.ch>
2015-04-14 13:58:24 +0200

    gr1: ok, except texture

--- tuto-wikibooks/gr1/main.go
+++ tuto-wikibooks/gr1/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
 	"runtime"
 
 	"github.com/go-gl/glfw/v3.1/glfw"
 	"github.com/sbinet/go-tuto-gl/tuto-wikibooks/glh"
+	"golang.org/x/mobile/f32"
 	"golang.org/x/mobile/gl"
 	"golang.org/x/mobile/gl/glutil"
 )
-
-type Point struct {
-	x float32
-	y float32
-}
 
 type context struct {
 	w    *glfw.Window
@@ -26,21 +28,13 @@ type context struct {
 
 	tex gl.Texture
 	vbo gl.Buffer
+	img *glutil.Image
+}
 
-	posbuf  gl.Buffer
-	pos     gl.Attrib
-	posdata []float32
-
-	colbuf  gl.Buffer
-	col     gl.Attrib
-	coldata []float32
-
-	eltbuf  gl.Buffer
-	elt     gl.Attrib
-	eltdata []uint16
-
-	fade  gl.Uniform
-	trans gl.Uniform
+func (ctx *context) Delete() {
+	gl.DeleteProgram(ctx.prog)
+	gl.DeleteBuffer(ctx.vbo)
+	ctx.w.Destroy()
 }
 
 func init() {
@@ -52,8 +46,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer glh.Delete()
+
+	w.SetKeyCallback(onKey)
 
 	prog, err := glutil.CreateProgram(vtxShader, fragShader)
+	if err != nil {
+		panic(err)
+	}
+
+	img, err := png.Decode(bytes.NewReader(MustAsset("res_texture.png")))
 	if err != nil {
 		panic(err)
 	}
@@ -69,17 +71,31 @@ func main() {
 
 		tex: gl.CreateTexture(),
 		vbo: gl.CreateBuffer(),
+		img: newImage(img),
 	}
+	defer ctx.Delete()
 
-	gl.ActiveTexture(gl.TEXTURE0)
-	gl.BindTexture(gl.TEXTURE_2D, ctx.tex)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-	gl.TexImage2D(
-		gl.TEXTURE_2D, 0, 15, 15, gl.RGBA, gl.UNSIGNED_BYTE, _res_texture_png,
-	)
+	//	gl.ActiveTexture(gl.TEXTURE0) gl.BindTexture(gl.TEXTURE_2D, ctx.tex)
+	//	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	//	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	//	gl.TexImage2D( gl.TEXTURE_2D, 0, 15, 15, gl.RGBA, gl.UNSIGNED_BYTE,
+	//	_res_texture_png,)
 
 	gl.BindBuffer(gl.ARRAY_BUFFER, ctx.vbo)
+	points := make([]float32, npoints*2)
+	for i := 0; i < npoints; i += 2 {
+		//		x := float32((i - npoints/2.0) / (npoints / 1))
+		x := float32((i - npoints/2) / (npoints / 200))
+		y := f32.Sin(x*10) / (1 + x*x)
+
+		points[i] = x
+		points[i+1] = y
+	}
+	gl.BufferData(
+		gl.ARRAY_BUFFER,
+		f32.Bytes(binary.LittleEndian, points...),
+		gl.STATIC_DRAW,
+	)
 
 	for !ctx.w.ShouldClose() {
 		display(ctx)
@@ -88,11 +104,104 @@ func main() {
 }
 
 func display(ctx context) {
+	// clear the background as black
+	gl.ClearColor(1, 1, 1, 1)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+
+	gl.UseProgram(ctx.prog)
+
+	gl.Uniform1i(ctx.texture, 0)
+	gl.Uniform1f(ctx.xoff, xoff)
+	gl.Uniform1f(ctx.xscale, xscale)
+
+	gl.BindBuffer(gl.ARRAY_BUFFER, ctx.vbo)
+
+	gl.EnableVertexAttribArray(ctx.coord)
+	gl.VertexAttribPointer(ctx.coord, 2, gl.FLOAT, false, 0, 0)
+
+	switch mode {
+	case 0:
+		gl.Uniform1f(ctx.sprite, 0)
+		gl.DrawArrays(gl.LINE_STRIP, 0, npoints)
+
+	case 1:
+		gl.Uniform1f(ctx.sprite, 1)
+		gl.DrawArrays(gl.POINTS, 0, npoints)
+
+	case 2:
+		ctx.img.Upload()
+		gl.Uniform1f(ctx.sprite, float32(ctx.img.Bounds().Dy()))
+		gl.DrawArrays(gl.POINTS, 0, npoints)
+	}
+
+	gl.DisableVertexAttribArray(ctx.coord)
+
+	// display result
+	ctx.w.SwapBuffers()
 }
 
+func onKey(w *glfw.Window, key glfw.Key, scancode int,
+	action glfw.Action, mods glfw.ModifierKey) {
+	switch {
+	case key == glfw.KeyEscape && action == glfw.Press,
+		key == glfw.KeyQ && action == glfw.Press:
+		w.SetShouldClose(true)
+		return
+	}
+
+	if action == glfw.Press {
+		switch key {
+		case glfw.KeyF1:
+			fmt.Printf("now drawing using lines...\n")
+			mode = 0
+			return
+
+		case glfw.KeyF2:
+			fmt.Printf("now drawing using points...\n")
+			mode = 1
+			return
+
+		case glfw.KeyF3:
+			fmt.Printf("now drawing using point sprites...\n")
+			mode = 2
+			return
+
+		case glfw.KeyLeft:
+			xoff -= 0.1
+
+		case glfw.KeyRight:
+			xoff += 0.1
+
+		case glfw.KeyUp:
+			xscale *= 1.5
+		case glfw.KeyDown:
+			xscale /= 1.5
+
+		case glfw.KeyHome:
+			xoff = 0
+			xscale = 1
+		}
+	}
+}
+
+func newImage(src image.Image) *glutil.Image {
+	b := src.Bounds()
+	img := glutil.NewImage(b.Dx(), b.Dy())
+	draw.Draw(img.RGBA, b, src, src.Bounds().Min, draw.Src)
+	img.Upload()
+	return img
+}
+
+var (
+	xoff   = float32(0)
+	xscale = float32(1)
+	mode   = 0
+)
+
 const (
-	width  = 800
-	height = 600
+	width   = 800
+	height  = 600
+	npoints = 20000
 
 	vtxShader = `
 #version 120
